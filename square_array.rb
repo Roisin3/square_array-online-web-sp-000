fd82f10e6d2a9e1e37e0c4115d15266b7238f55d
Michael Lynch <michael@michaelrlynch.com>
2019-08-12 01:25:32 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,9 +1,7 @@
 def square_array(array)
   new_array = []
   array.each do |num|
-      new_array << num **2
-      puts new_array
+    new_array << num **2
+    puts new_array
     end
   end
-
-  square_array(array)
